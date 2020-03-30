8777e5dd183ce74b457bf83d0c269d0767ba2b56
Tom Forbes <tom@tomforb.es>
2020-03-30 22:16:26 +0100

    Just use ureq

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,26 +1,10 @@
-//use std::collections::HashMap;
-// use serde::{Serialize, Serializer};
-//
-// struct X;
-//
-// impl Serialize for X {
-//   fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-//     serializer.serialize_str("Hello, world!")
-//   }
-// }
-//
-//fn main() {
-//  println!("{}", serde_json::to_string(&X).unwrap());
-//}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // println!("{}", serde_json::to_string(&X).unwrap());
-  let body = reqwest::get("https://www.rust-lang.org")
-    .await.expect("error fetching")
-    .text()
-    .await;
+  let resp = ureq::post("http://my-server.com/ingest")
+    .set("Transfer-Encoding", "chunked")
+    .send_string("Hello world");
 
-  println!("{:?}", body);
+  println!("{:?}", resp);
   Ok(())
 }
