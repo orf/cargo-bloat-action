8b9aa37b14e158f546dda94be090650d3256682a
Tom Forbes <tom@tomforb.es>
2020-03-30 22:19:17 +0100

    Add json to ureq

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,10 +1,15 @@
+use ureq::json;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  // println!("{}", serde_json::to_string(&X).unwrap());
-  let resp = ureq::post("http://my-server.com/ingest")
-    .set("Transfer-Encoding", "chunked")
-    .send_string("Hello world");
+    // println!("{}", serde_json::to_string(&X).unwrap());
+    let resp = ureq::post("http://my-server.com/ingest")
+        .set("Transfer-Encoding", "chunked")
+        .send_json(json!({
+            "name": "martin",
+            "rust": true
+        }));
 
-  println!("{:?}", resp);
-  Ok(())
+    println!("{:?}", resp.into_json());
+    Ok(())
 }
