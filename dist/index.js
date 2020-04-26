8a8b675fe528908c08a1f69d24380301f5e7db2d
Tom Forbes <tom@tomforb.es>
2020-04-26 19:52:58 +0100

    Replace

--- dist/index.js
+++ dist/index.js
@@ -5672,12 +5672,12 @@ function treeToDisplay(tree) {
     const treeObject = {};
     const currentKeyPath = [];
     tree.split('\n').forEach(line => {
-        const found = line.match(/^(\d+)(.*) (\(\*\))?/);
+        const found = line.match(/^(\d+)(.*)?/);
         if (found == null) {
             return;
         }
         const indent = parseInt(found[1], 10) - 1;
-        const ourKey = found[2];
+        const ourKey = found[2].replace("(*)", "");
         if (indent + 1 > currentKeyPath.length) {
             currentKeyPath.push(ourKey);
         }
